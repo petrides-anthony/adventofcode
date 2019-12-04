5fdf68a5badc5fda573fc7e72cb60b59a9d3a98e
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-12-04 13:41:06 +0000

    Solution using reduce
    We don't know if this is more performant solution but it makes the code smaller
    and more declarative.
    
    https://codeburst.io/declarative-vs-imperative-programming-a8a7c93d9ad2?gi=212fc87c6a99

--- day1/fuel-calculator.js
+++ day1/fuel-calculator.js
@@ -1,18 +1,22 @@
 const allModules = require('./all-modules');
 
-// Logic: divide by three, round down, and subtract 2
-
 const totalFuel = () => {
-    let totalFuel = 0
-    allModules.forEach((fuelModule) => {
-     totalFuel += fuelRequired(fuelModule)
-    })
-  return totalFuel
+    // let totalFuel = 0
+    // allModules.forEach((fuelModule) => {
+    //     totalFuel += (fuelModule)
+    // });
+    // Below is the same as doing the above but using 'Reduce' which bakes in the summing part
+    // return allModules.reduce( (totalFuel, fuelModule) => {
+    //     return totalFuel + fuelRequired(fuelModule);
+    // }, 0);
+
+    return allModules.reduce( (totalFuel, fuelModule) => totalFuel + fuelRequired(fuelModule), 0);
 }
 
 function fuelRequired(mass) {
     return Math.floor(mass / 3) - 2
   }
+
   module.exports = {
     fuelRequired,
     totalFuel
