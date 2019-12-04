100622dfbeb2428627936743029900808f16e6a8
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-12-04 13:19:29 +0000

    Complete Day One Exercise with jest tests
    Ready to make some cleanup but this keeps us going.

--- day1/fuel-calculator.js
+++ day1/fuel-calculator.js
@@ -1,4 +1,23 @@
-function sum(a, b) {
-    return a + b;
+const allModules = require('./all-modules');
+
+// Logic: divide by three, round down, and subtract 2
+
+const totalFuel = () => {
+    let totalFuel = 0
+    allModules.forEach((fuelModule) => {
+     totalFuel += fuelRequired(fuelModule)
+    })
+  return totalFuel
+}
+
+function fuelRequired(mass) {
+    return Math.floor(mass / 3) - 2
   }
-  module.exports = sum;
+  module.exports = {
+    fuelRequired,
+    totalFuel
+  }
+
+ 
+
+//   arr.forEach(callback(currentValue [, index [, array]])[, thisArg]);
--- day1/fuel-calculator.spec.js
+++ day1/fuel-calculator.spec.js
@@ -1,5 +1,21 @@
-const sum = require('./fuel-calculator');
+const {fuelRequired, totalFuel} = require('./fuel-calculator');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('fuel required for a mass of 12 is 2', () => {
+  expect(fuelRequired(12)).toBe(2);
 });
+
+test('fuel required for a mass of 14 is 2', () => {
+    expect(fuelRequired(14)).toBe(2);
+});
+
+  test('fuel required for a mass of 1969 is 654', () => {
+    expect(fuelRequired(1969)).toBe(654);
+  });
+
+  test('fuel required for a mass of 100756 is 33583', () => {
+    expect(fuelRequired(100756)).toBe(33583);
+  });
+
+  test('calculate total fuel required for all the spacecraft', () => {
+    expect(totalFuel()).toBe(3256794);
+  });
